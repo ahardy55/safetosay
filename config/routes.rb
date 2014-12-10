f574ee7ed4034b95d86967cd93f4fca7ff4b1566
Adam Hardy <ahardy55@gmail.com>
2014-12-10 14:48:51 -0600

    added nav links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :photos
+
   resources :musics
 
   get 'home/index'
@@ -14,6 +16,10 @@ Rails.application.routes.draw do
   resources :news_updates
 
   resources :home
+
+  resources :musics
+
+  resources :photos
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141111200733) do
+ActiveRecord::Schema.define(version: 20141210204501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,9 +32,22 @@ ActiveRecord::Schema.define(version: 20141111200733) do
     t.datetime "updated_at"
   end
 
+  create_table "musics", force: true do |t|
+    t.string   "link"
+    t.string   "title"
+    t.string   "release_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "news_updates", force: true do |t|
     t.text     "content"
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
